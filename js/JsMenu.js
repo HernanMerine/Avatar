a7c6e6e4aea5e732d19741fd76ddbe2ffcaece85
HernanMerine <128190738+HernanMerine@users.noreply.github.com>
2023-10-25 12:33:23 -0300

    menu desplegable

---
+++ js/JsMenu.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const personajesContainer = document.querySelector(".Personajes-container");
+    const personajesContent = document.querySelector(".Personajes-content");
+
+    personajesContainer.addEventListener("click", function (event) {
+        event.stopPropagation();
+        if (personajesContent.style.display === "none" || personajesContent.style.display === "") {
+            personajesContent.style.display = "block";
+            document.querySelector('.arrow').textContent = '▼'; // Cambia la flecha a abajo
+        } else {
+            personajesContent.style.display = "none";
+            document.querySelector('.arrow').textContent = '▶'; // Cambia la flecha a derecha
+        }
+    });
+
+    document.addEventListener("click", function () {
+        personajesContent.style.display = "none";
+        document.querySelector('.arrow').textContent = '▶'; // Cambia la flecha a derecha
+    });
+
+    personajesContent.addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
+});
+
